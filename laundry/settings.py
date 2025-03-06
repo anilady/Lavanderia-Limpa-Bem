d67706560f86d7085ebbfbfc551a6cf725b7f587
Daniela Baliza <daniinhasinnha@gmail.com>
2025-03-06 17:08:38 -0300

    Adiciona domínio 'lavanderia-limpa-bem.onrender.com' a ALLOWED_HOSTS

--- laundry/settings.py
+++ laundry/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-8rf)2eaz^0f%av9=x+75mypninkq$j*ivrf*=$rk%gry884s6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lvandeiria-limpa-bem.onrender.com', 'localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['lavanderia-limpa-bem.onrender.com', 'localhost', '127.0.0.1']
 
 
 
